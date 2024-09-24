af32bf5ed899ededfc4d725ccac4581d324dc35c
Anuraag Agrawal <anuraaga@gmail.com>
2024-09-24 10:36:42 +0900

    Update plugin entry point for latest version

--- buildtools/wasm/protoc-gen-connect-es.ts
+++ buildtools/wasm/protoc-gen-connect-es.ts
@@ -1,6 +1,6 @@
 import "./textcoding.js";
 
-import { protocGenConnectEs } from "@connectrpc/protoc-gen-connect-es/src/protoc-gen-connect-es-plugin.js";
+import { protocGenConnectEs } from "@connectrpc/protoc-gen-connect-es/dist/cjs/src/protoc-gen-connect-es-plugin.js";
 
 import { runQuickJs  } from "./run-quickjs.js";
 
